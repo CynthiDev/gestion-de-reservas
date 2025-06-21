b66d709ebf99c1cc47058baeaa358cc48d3cd63e
CynthiaCG <cynthiaestef@gmail.com>
2025-06-21 14:25:49 -0300

    fix get and delete  reservas,   creacion de variables de entorno y actualizacion de dependencias jwt y jsonwebtocken

--- src/controllers/reservationController.js
+++ src/controllers/reservationController.js
@@ -2,6 +2,7 @@ import * as reservationService from '../services/reservationService.js';
 const MSG_ERROR_500 = 'Error interno del servidor. Por favor, inténtalo de nuevo más tarde.';
 const HEADER_ACCEPT = 'accept';
 const ACCEPT_POSTMAN = '*/*';
+import jwt from 'jsonwebtoken'
 
 export async function crearReserva( req, res) {
     try {
@@ -87,9 +88,10 @@ export async function actualizarEstadoReserva (req, res) {
 export async function eliminarReserva (req, res) {
     try {
         const reservas = await reservationService.eliminarReservaPorId(req.params.id);
-        if (!reservas || reservas.length === 0) {
+        if (!reservas) {
             return res.status(404).json({ message: `No se encontró la reserva con id: ${req.params.id}` });
         }
+        
         return res.json(reservas);
     } catch (error) {
         console.error("Error en el controlador:", error);
@@ -111,8 +113,17 @@ export async function obtenerFechaTurnosDisponibles (req, res) {
         if (req.get(HEADER_ACCEPT) === ACCEPT_POSTMAN) { //valor seteado desde Postman
             return res.json(fechaTurnosDisponibles);
         }
+        const token= req.cookies.token;
+        if (!token) {
+            return res.status(403).send('Acceso no autorizado')
+        }
+        try {
+            const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            return res.render('newReservation', {fechasDisponibles: fechaTurnosDisponibles, user: data});
+        } catch (error) {
+            return res.status(403).send('Acceso no autorizado 2')
+        }
 
-        return res.render('newReservation', {fechasDisponibles: fechaTurnosDisponibles, user: req.session.user});
     } catch (error) {
         console.error("Error en el controlador:", error);
         if (error.isClientError) {
